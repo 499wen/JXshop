2dc0fe13679fc83a6673adef539119bf6f2c9fa3
wen223738 <wen2237380164@126.com>
2018-09-25 19:36:51 +0800

    整合页面

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -3,9 +3,27 @@ namespace controllers;
 
 class IndexController {
 
-
+    // 首页页面
     function index (){
         
         view("index/index");
+    }
+
+    // top
+    function top (){
+
+        view("index/top");
+    }
+
+    // menu
+    function menu (){
+        
+        view("index/menu");
+    }
+
+    // main
+    function main (){
+
+        view("index/main");
     }
 }
